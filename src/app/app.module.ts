2c506867e07499b81355aa53fb414db8eb922d53
Abdullah Aljarrah <aaljarrah3@gmail.com>
2024-07-21 22:14:57 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule } from '@angular/router';
+//import { BuilderModule } from '@builder.io/angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+  ]
   providers: [
     provideClientHydration()
   ],
