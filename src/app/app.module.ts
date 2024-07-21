abb18dfd34d8fff6d08462ac0b7047a236191319
Abdullah Aljarrah <aaljarrah3@gmail.com>
2024-07-21 22:27:32 +0300

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RouterModule } from '@angular/router';
-//import { BuilderModule } from '@builder.io/angular';
+import { BuilderModule } from '@builder.io/angular';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,14 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BuilderModule.forRoot("2107d529dde941029e22c1a062500866"), // <-- import here and add API Key
+    RouterModule.forRoot([
+      {
+        path: '**',
+        component: LandingPageComponent, // <-- add LandingPageComponent here
+      },
+    ]),
   ],
   providers: [
     provideClientHydration()
